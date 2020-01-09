74eb466681e12e119aec57f679001a11cabfe891
PROG1400_SourceCode <sos8323@gmail.com>
2020-01-09 12:17:55 -0400

    changed to display a number

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -4,7 +4,13 @@
             // psvm is shortcut to make template for main source code
             // Print out Hello world
 
-            System.out.println("Hello World!!");
+            // declare variable
+            int num;
+            num = 123456;
+
+
+
+            System.out.println("num="+num);
             // sout is shortcut to make template for print out code
         }
     }
