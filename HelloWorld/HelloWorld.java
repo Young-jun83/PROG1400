d018aed0b3e09f4717133b3f35deb18bbec31fcb
PROG1400_SourceCode <sos8323@gmail.com>
2020-01-09 11:46:37 -0400

    Hello World

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+    class HelloWorld {
+
+        public static void main(String[] args) {
+            // psvm is shortcut to make template for main source code
+            // Print out Hello world
+
+            System.out.println("Hello World!!");
+            // sout is shortcut to make template for print out code
+
+        }
+    }
